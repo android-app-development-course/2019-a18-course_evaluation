fc5cc7ee953a3958adab58b07806a214395ba293
zpp3668 <53029132+zpp3668@users.noreply.github.com>
2019-12-14 13:09:54 +0800

    解决修改密码后出现的问题

--- app/src/main/java/com/scottmangiapane/courseevaluation/ui/my_info/MyInfoFragment.java
+++ app/src/main/java/com/scottmangiapane/courseevaluation/ui/my_info/MyInfoFragment.java
@@ -12,7 +12,6 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.alibaba.fastjson.JSON;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
